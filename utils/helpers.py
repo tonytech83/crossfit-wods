18a4a910f947dc03367c30c7aabd8a8c471741ba
tonytech83 <a.petrov.83@gmail.com>
2025-01-05 17:55:14 +0200

    change view on root, from ul to flex div with cubes. separate colors in different css file. add others :)

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,24 @@
 import os
+from datetime import datetime
 
 data_dir = 'data'
 
-def get_data_dir():
+def format_date_from_filename(filename):
+    date_part = filename.split('.')[0]  # "2022-06-21"
+    date_obj = datetime.strptime(date_part, "%Y-%m-%d")
 
-    data_files = [file for file in os.listdir(data_dir) if file.endswith('.html')]
+    return date_obj.strftime("%d %B %Y")
+
+def get_data_dir():
+    """
+    Create list of dictionaries with file name and from file name which is date formated dare
+    :return: list of dictionaries
+    """
+    data_files = [
+        {"filename": file, "formatted_date": format_date_from_filename(file)}
+        for file in os.listdir(data_dir)
+        if file.endswith('.html')
+    ]
 
     return data_files
 
